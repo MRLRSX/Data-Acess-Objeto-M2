0839a6a4f57d581b68c8b430e9ceba9a704e06d4
MRLRSX <lucas.rocha-dev@outlook.com>
2022-09-23 02:11:28 -0300

    final

--- src/application/Program_01.java
+++ src/application/Program_01.java
@@ -1,28 +1,6 @@
 package application;
 
-import java.util.function.Consumer;
-
-import model.Cidades;
-import model.Prefeitos;
-import model.dao.DaoFactory;
-import model.dao.PrefeitosDao;
-
 public class Program_01 {
 	public static void main(String[] args) {
-       PrefeitosDao pDAO = DaoFactory.createPrefeitosDaoJDBC();
-       Consumer<Object> print = System.out::println;
-       
-       
-       System.out.println(pDAO.findById(1L));
-       System.out.println("\n");
-       System.out.println("PREFEITO -> ");
-       pDAO.findAll().stream().forEach(print);
-       System.out.println("cidade -> ");
-       pDAO.findAll().stream().map(c -> c.getCidades()).forEach(print);
-       System.out.println("ESTADO -> ");
-       pDAO.findAll().stream().map(c -> c.getCidades().getEstados()).forEach(print);
-       pDAO.deleteById(1L);
-       Cidades cidades = pDAO.findById(2L).getCidades();
-       pDAO.update(new Prefeitos(2L, "LUCAS", cidades));
 	}
 }
--- src/model/implementacao/EmpresasDaoJDBC.java
+++ src/model/implementacao/EmpresasDaoJDBC.java
@@ -1,14 +1,20 @@
 package model.implementacao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import db.Conexao;
+import db.DBException;
 import model.Empresas;
 import model.dao.EmpresasDao;
 
 public class EmpresasDaoJDBC implements EmpresasDao {
 
-	private static Connection connection = null;
+	private Connection connection = null;
 
 	public EmpresasDaoJDBC(Connection connection) {
 		this.connection = connection;
@@ -16,32 +22,99 @@ public class EmpresasDaoJDBC implements EmpresasDao {
 
 	@Override
 	public void insert(Empresas empresas) {
-		// TODO Auto-generated method stub
+		PreparedStatement prepared = null;
+		try {
+			prepared = connection.prepareStatement("INSERT INTO tb_empresas (nome_empresa, cnpj_empresa) VALUES(?, ?)");
+			prepared.setString(1, empresas.getNome());
+			prepared.setString(2, empresas.getCnpj());
+			prepared.executeUpdate();
+		} catch (SQLException erro) {
+			throw new DBException(erro.getMessage());
+		} finally {
+			Conexao.closeStatement(prepared);
+		}
 
 	}
 
 	@Override
 	public Empresas findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement prepared = null;
+		ResultSet result = null;
+		Empresas empresa = new Empresas();
+		try {
+			prepared = connection.prepareStatement("SELECT * FROM tb_empresas WHERE id_empresa = ?");
+			prepared.setLong(1, id);
+			result = prepared.executeQuery();
+			while (result.next()) {
+				empresa.setId(result.getLong("id_empresa"));
+				empresa.setNome(result.getString("nome_empresa"));
+				empresa.setCnpj(result.getString("cnpj_empresa"));
+			}
+			return empresa;
+		} catch (SQLException erro) {
+			throw new DBException(erro.getMessage());
+		} finally {
+			Conexao.closeResultSet(result);
+			Conexao.closeStatement(prepared);
+		}
 	}
 
 	@Override
 	public void update(Empresas empresas) {
-		// TODO Auto-generated method stub
+		PreparedStatement prepared = null;
+		try {
+			prepared = connection
+					.prepareStatement("UPDATE tb_empresas SET nome_empresa = ?, cnpj_empresa = ? WHERE id_empresa = ?");
+			prepared.setString(1, empresas.getNome());
+			prepared.setString(2, empresas.getCnpj());
+			prepared.setLong(3, empresas.getId());
+			prepared.executeUpdate();
+		} catch (SQLException erro) {
+			throw new DBException(erro.getMessage());
+		} finally {
+			Conexao.closeStatement(prepared);
+		}
 
 	}
 
 	@Override
 	public void deleteById(Long id) {
-		// TODO Auto-generated method stub
+		PreparedStatement prepared = null;
+		try {
+			prepared = connection.prepareStatement("DELETE FROM tb_empresas WHERE id_empresa = ?");
+			prepared.setLong(1, id);
+			prepared.executeUpdate();
+		} catch (SQLException erro) {
+			throw new DBException(erro.getMessage());
+		} finally {
+			Conexao.closeStatement(prepared);
+		}
 
 	}
 
 	@Override
 	public List<Empresas> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+
+		PreparedStatement prepared = null;
+		ResultSet result = null;
+        List<Empresas> listaEmpresa = new ArrayList<>();
+		try {
+			prepared = connection.prepareStatement("SELECT * FROM tb_empresas");
+			result = prepared.executeQuery();
+			while (result.next()) {
+				Empresas empresa = new Empresas();
+				empresa.setId(result.getLong("id_empresa"));
+				empresa.setNome(result.getString("nome_empresa"));
+				empresa.setCnpj(result.getString("cnpj_empresa"));
+				listaEmpresa.add(empresa);
+			}
+			return listaEmpresa;
+		} catch (SQLException erro) {
+			throw new DBException(erro.getMessage());
+		} finally {
+			Conexao.closeResultSet(result);
+			Conexao.closeStatement(prepared);
+		}
 	}
 
 }
